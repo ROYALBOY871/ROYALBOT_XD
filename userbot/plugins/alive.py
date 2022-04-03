3e979aec3f89991359173577b401a3d3756de782
ROYALBOY <98400258+ROYALBOY871@users.noreply.github.com>
2022-04-03 17:22:05 +0530

    Create alive.py

---
+++ userbot/plugins/alive.py
@@ -0,0 +1,17 @@
+"""Check if userbot alive. If you change these, you become the gayest gay such that even the gay world will disown you."""
+import asyncio
+from telethon import events
+from telethon.tl.types import ChannelParticipantsAdmins
+from platform import uname
+from userbot import ALIVE_NAME
+from userbot.utils import admin_cmd
+
+DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Set ALIVE_NAME in config vars in Heroku"
+
+@command(outgoing=True, pattern="^.alive$")
+async def amireallyalive(alive):
+    """ For .alive command, check if the bot is running.  """
+    await alive.edit("`𝙍𝙊𝙔𝘼𝙇𝘽𝙊𝙏 𝙄𝙎 𝙊𝙉𝙇𝙄𝙉𝙀 \n 𝙄𝙣𝙛𝙤:- \n\nYes Sir ! I'm Alive\n\nTelethon version: 6.9.0\nPython: 3.7.3\n\n`"
+                    f"`My Master`: {DEFAULTUSER}\n"
+                     "`My Owner`: @ROYALBOY_XD\n\n"
+                     "Join [Channel](https://t.me/ROYALYSERBOT/) For Latest Updates")
